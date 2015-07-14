8c6a8d599b01e4207ed759794daee76c6418adf7
Michael Lynch <nycynik@gmail.com>
2015-07-13 23:05:30 -0400

    first commit

---
+++ 3Cs/Assets/GameCam.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/***
+ * GameCam Class
+ * 
+ * 3rd person canera class.
+ * Follows the player, keeping player in view.
+ * Uses linear interpolation and dampening to keep the camera facing the right
+ * way and not joslign around.
+ * 
+ */
+public class GameCam : MonoBehaviour {
+	
+	public GameObject trackObj;  // What to track, the player mostly.
+	public float height;
+	public float desiredDistance; // how far behind.
+	public float heightDamp;
+	public float rotDamp;
+
+	float DesiredRotationalAngle;
+	float DesiredHeight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void UpdateRotAndTrans ()
+	{
+
+
+		if (trackObj) {
+			// tracked object
+			// y is horizontal angle of the object
+			DesiredRotationalAngle = trackObj.transform.eulerAngles.y;
+			DesiredHeight = trackObj.transform.position.y + height;
+
+			// camera
+			float RotAngle = transform.eulerAngles.y;
+			float Height = transform.position.y;
+
+			RotAngle = Mathf.LerpAngle (RotAngle, DesiredRotationalAngle, rotDamp);
+			Height = Mathf.LerpAngle (Height, DesiredHeight, heightDamp * Time.deltaTime); // depends on real time, not frame rate
+
+			// move camera in the opposite direction to where player is facing (so we can put the cam behind the player)
+			Quaternion CurrentRotation = Quaternion.Euler(0.0f, RotAngle, 0.0f);
+			Vector3 pos = trackObj.transform.position;
+			pos -= CurrentRotation * Vector3.forward * desiredDistance;
+			pos.y = Height;
+
+			// set the whole thing all at once, only way.
+			transform.position = pos;
+
+			transform.LookAt(trackObj.transform.position);
+
+		} else {
+			Debug.Log("[900] Game camera Error: nothing to track.");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		UpdateRotAndTrans();
+	}
+}
---
+++ 3Cs/Assets/PlayerControls.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+	public Vector3 moveDirection = Vector3.zero;
+
+	public float rotateSpeed;
+	public float moveSpeed = 0.0f;
+	private float speedSmoothing = 10.0f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void UpdateMovement ()
+	{
+		Vector3 cameraForward = Camera.main.transform.TransformDirection(Vector3.forward);
+
+		cameraForward.y = 0.0f;
+		cameraForward.Normalize();
+
+		// TIP: we find the right vector by flipping the x and z components and negating the last component
+		// faster than extracting and transforming the right vector. (normally x,y,z,so flipped is z 0 x)
+		Vector3 cameraRight = new Vector3(cameraForward.z, 0.0f, -cameraForward.x);
+
+		float v = Input.GetAxisRaw("Vertical");
+		float h = Input.GetAxisRaw("Horizontal");
+
+		// target direction
+		Vector3 targetDirection = ((h * cameraRight) + (v * cameraForward));
+
+		moveDirection = Vector3.RotateTowards (moveDirection, targetDirection, rotateSpeed * Mathf.Deg2Rad * Time.deltaTime, 1000);
+		moveDirection = moveDirection.normalized;
+
+		// smoth the speed
+		float curSmooth = speedSmoothing * Time.deltaTime;
+		float targetSpeed = Mathf.Min (targetDirection.magnitude, 1.0f);
+		moveSpeed = Mathf.Lerp(moveSpeed, targetSpeed, curSmooth);
+
+		// displacement vector
+		Vector3 displacement = moveDirection * moveSpeed * Time.deltaTime;
+
+		// move character.
+		this.GetComponent<CharacterController>().Move(displacement);
+		transform.rotation = Quaternion.LookRotation (moveDirection);
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateMovement();
+	}
+}
